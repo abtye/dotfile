d22a7f1661a44bcce6c6d955aa5e243178c6ffce
abtye <xin3142022@126.com>
2024-12-08 14:24:53 +0800

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# 自用 Arch Linux 配置
---
+++ package.bash
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+sudo pacman -S --needed sof-firmware
+sudo pacman -S --needed mpv vim git fzf fish ffmpeg fuse2 libdecor libnotify grim slurp
+sudo pacman -S --needed foot wofi mako jq wlogout brightnessctl nwg-look xdg-desktop-portal-wlr xdg-desktop-portal-gtk waybar wl-clipboard mate-polkit qt6ct fcitx5-im fcitx5-rime sound-theme-freedesktop zenity
+sudo pacman -S --needed vulkan-intel vpl-gpu-rt intel-compute-runtime intel-media-driver intel-ucode
+sudo pacman -S --needed adobe-source-han-sans-cn-fonts adobe-source-han-sans-kr-fonts otf-codenewroman-nerd otf-font-awesome ttf-twemoji noto-fonts
